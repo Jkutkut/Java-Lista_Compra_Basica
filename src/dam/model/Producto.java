da70712245ba22f47b9325cfd06b33f6bf561459
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-07 13:30:26 +0200

    producto con validación

--- src/dam/model/Producto.java
+++ src/dam/model/Producto.java
@@ -1,5 +1,6 @@
 package dam.model;
 
+import dam.exception.InvalidDataException;
 import dam.model.UnidadesProducto;
 
 public class Producto {
@@ -16,17 +17,33 @@ public class Producto {
 
     // SETTERS
     private void setNombre(String nombre) {
+        if (nombre == null || nombre.isEmpty()) {
+            throw new InvalidDataException("El nombre no puede ser nulo o vacío");
+        }
         this.nombre = nombre;
     }
 
     private void setCantidad(int cantidad) {
         if (cantidad < 0) {
-            
+            throw new InvalidDataException("La cantidad no puede ser negativa");
         }
         this.cantidad = cantidad;
     }
 
     private void setUnidad(String unidad) {
+        if (!UnidadesProducto.isUnidad(unidad)) {
+            throw new InvalidDataException("La unidad no es válida");
+        }
         this.unidad = unidad;
+    }
+
+    // GETTERS
+
+    @Override
+    public String toString() {
+        return String.format(
+            "%s: %d %s",
+            nombre, cantidad, unidad
+        );
     }
 }
