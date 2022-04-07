231779811189cfee54ca2ea942d6c9735eefe1e0
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-07 13:56:28 +0200

    unidades added

--- src/dam/view/View.java
+++ src/dam/view/View.java
@@ -1,22 +1,25 @@
 package dam.view;
 
 import dam.control.Controller;
+import dam.model.UnidadesProducto;
 
 import javax.swing.*;
 
 public class View extends JFrame {
     private JPanel menu;
-    private JLabel lblTitle;
-    private JTextField txtFProducto;
-    private JSpinner spCantidad;
-    private JComboBox cmbUnidad;
-    private JButton eliminarButton;
-    private JButton añadirButton;
-    private JList listProductos;
+    private JPanel panelControl;
     private JLabel lvlProducto;
     private JLabel lvlCantidad;
     private JLabel lvlUnidad;
-    private JPanel panelControl;
+    private JLabel lblTitle;
+
+    private JTextField txtFProducto;
+    private JSpinner spCantidad;
+    private JComboBox cmbUnidad;
+    private JButton añadirButton;
+    private JButton eliminarButton;
+    private JList listProductos;
+
 
     public View() {
         setContentPane(menu);
@@ -28,7 +31,8 @@ public class View extends JFrame {
     }
 
     private void initComponents() {
-
+        // Units
+        cmbUnidad.setModel(new DefaultComboBoxModel(UnidadesProducto.getUnidades()));
     }
 
     public void setControlador(Controller controller) {
